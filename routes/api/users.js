5de69230c04ed0b31487ed48b6867ad1ca5b2a71
FSDonnelly <donnelly_frank@yahoo.com>
2019-07-22 23:59:11 -0700

    GET current user profile

--- routes/api/users.js
+++ routes/api/users.js
@@ -82,7 +82,7 @@ router.post(
       );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server Error');
+      res.status(500).send('User Server Error');
     }
   }
 );
