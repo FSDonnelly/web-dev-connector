9e47e0c618411cd9ba7ca78cc22a65f400bb4c17
FSDonnelly <donnelly_frank@yahoo.com>
2019-08-02 17:06:45 -0700

    resolved some alerts

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -194,7 +194,7 @@ export const deleteEducation = id => async dispatch => {
 export const deleteAccount = () => async dispatch => {
   if (window.confirm('Are you sure? This can not be undone!')) {
     try {
-      const res = await axios.delete(`/api/profile`);
+      await axios.delete(`/api/profile`);
 
       dispatch({ type: CLEAR_PROFILE });
 
