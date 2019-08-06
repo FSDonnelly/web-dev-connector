20dc36f03cc98b5286386c9f2a003a9eb3a91344
FSDonnelly <donnelly_frank@yahoo.com>
2019-08-05 18:04:09 -0700

    like and unlike functionality

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -27,7 +27,7 @@ export default function(state = initialState, action) {
       return {
         ...state,
         posts: state.posts.map(post =>
-          post._id === payload.id ? { ...post, likes: payload.likes } : post
+          post._id === payload.postId ? { ...post, likes: payload.likes } : post
         ),
         loading: false
       };
