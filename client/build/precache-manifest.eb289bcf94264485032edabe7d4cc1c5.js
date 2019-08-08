04c7685bf5c7a56fcd5a65f64d4271f3a7cae6d3
FSDonnelly <donnelly_frank@yahoo.com>
2019-08-07 23:05:08 -0700

    deploy

---
+++ client/build/precache-manifest.eb289bcf94264485032edabe7d4cc1c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc3dd25dc6272488f0e180aba2bbce2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64f9a5cf7aceff234c6e",
+    "url": "/static/css/main.11b62bc3.chunk.css"
+  },
+  {
+    "revision": "299cb18eb708538c0418",
+    "url": "/static/js/2.40cf7465.chunk.js"
+  },
+  {
+    "revision": "64f9a5cf7aceff234c6e",
+    "url": "/static/js/main.062385fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "2b018964331551ec6c27f81efc8b160c",
+    "url": "/static/media/spinner.2b018964.gif"
+  }
+]);
